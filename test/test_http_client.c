1486064b8af77c95aa2df6082bcac7ddc10976e2
Taylor Lowery <1682932+taylorlowery@users.noreply.github.com>
2026-06-11 14:32:46 -0700

    Support headers in http-client, update tests.

--- test/test_http_client.c
+++ test/test_http_client.c
@@ -76,7 +76,6 @@ void test_post_to_invalid_endpoint_returns_error_status(void) {
     TEST_ASSERT_NOT_NULL(resp);
     if (resp) {
         TEST_ASSERT_EQUAL_INT(405, resp->status_code);
-        fprintf(stdout, "post response body: %s", resp->body);
         free(resp->body);
         free(resp);
     }
@@ -107,10 +106,9 @@ void test_http_client_request_post(void) {
         TEST_ASSERT_EQUAL_INT(200, resp->status_code);
         // TODO: parse the response headers as HttpHeader structs
         // validate headers present in response body as strings
-        TEST_ASSERT_NOT_NULL_MESSAGE(strstr(resp->body, "\"x-api-key\": \"deadbeef\""), "x-api-key header not found in response body");
-        TEST_ASSERT_NOT_NULL_MESSAGE(strstr(resp->body, "\"x-tenant-context\": \"1337\""), "x-tenant-context header not found in response body");
-        TEST_ASSERT_NOT_NULL_MESSAGE(strstr(resp->body, "\"test\": \"data\""), "test header not found in response body");
-        fprintf(stdout, "post response body: %s", resp->body);
+        TEST_ASSERT_NOT_NULL_MESSAGE(strcasestr(resp->body, "\"x-api-key\": \"deadbeef\""), "x-api-key header not found in response body");
+        TEST_ASSERT_NOT_NULL_MESSAGE(strcasestr(resp->body, "\"x-tenant-context\": \"1337\""), "x-tenant-context header not found in response body");
+        TEST_ASSERT_NOT_NULL_MESSAGE(strcasestr(resp->body, "\"test\": \"data\""), "test header not found in response body");
         free(resp->body);
         free(resp);
     }
@@ -140,10 +138,9 @@ void test_https_client_request_post(void) {
         TEST_ASSERT_EQUAL_INT(200, resp->status_code);
         // TODO: parse the response headers as HttpHeader structs
         // validate headers present in response body as strings
-        TEST_ASSERT_NOT_NULL_MESSAGE(strstr(resp->body, "\"x-api-key\": \"deadbeef\""), "x-api-key header not found in response body");
-        TEST_ASSERT_NOT_NULL_MESSAGE(strstr(resp->body, "\"x-tenant-context\": \"1337\""), "x-tenant-context header not found in response body");
-        TEST_ASSERT_NOT_NULL_MESSAGE(strstr(resp->body, "\"test\": \"data\""), "test header not found in response body");
-        fprintf(stdout, "post response body: %s", resp->body);
+        TEST_ASSERT_NOT_NULL_MESSAGE(strcasestr(resp->body, "\"x-api-key\": \"deadbeef\""), "x-api-key header not found in response body");
+        TEST_ASSERT_NOT_NULL_MESSAGE(strcasestr(resp->body, "\"x-tenant-context\": \"1337\""), "x-tenant-context header not found in response body");
+        TEST_ASSERT_NOT_NULL_MESSAGE(strcasestr(resp->body, "\"test\": \"data\""), "test header not found in response body");
         free(resp->body);
         free(resp);
     }
