29b084651d68e80200b875ccb372d4102167f1ca
Taylor Lowery <1682932+taylorlowery@users.noreply.github.com>
2026-07-25 13:18:15 -0700

    remove placeholder tests

--- test/test_agent.c
+++ test/test_agent.c
@@ -1,6 +1,4 @@
 #include "agent.h"
-#include "anthropic.h"
-#include <stdio.h>
 #include "vendor/unity/unity.h"
 #include "vendor/unity/unity_internals.h"
 
@@ -8,35 +6,12 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-// validate that my greet() function
-// prints the expected greeting to a given file pointer.
-void test_greet(void) {
-    char buffer[100];
-    FILE *fp = fmemopen(buffer, 100, "w+");
-    if (NULL == fp) {
-        fprintf(stderr, "Failed to open memory buffer for fmemopen\n");
-        return;
-    }
-
-    greet(fp);
-    fflush(fp);
-    rewind(fp);
-
-    char validationBuf[100];
-    if (fgets(validationBuf, sizeof(validationBuf), fp) == NULL) {
-        TEST_FAIL_MESSAGE("failed to read from buffer");
-    }
-    fclose(fp);
-    TEST_ASSERT_EQUAL_STRING("Howdy, Pilgrim!", validationBuf);
-}
 
 void test_run(void) {
-    Run();
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_greet);
     RUN_TEST(test_run);
     return UNITY_END();
 }
